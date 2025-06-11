e5f9717ae49cabcc60c0bcc57a3d0de6823822c6
xmohammad1 <117955130+xmohammad1@users.noreply.github.com>
2025-06-12 03:21:24 +0330

    Display data usage in GB (#40)

--- app/dashboard/src/utils/formatByte.tsx
+++ app/dashboard/src/utils/formatByte.tsx
@@ -11,6 +11,14 @@ export function formatBytes(bytes: number, decimals = 2, asArray = false) {
   else return [parseFloat((bytes / Math.pow(k, i)).toFixed(dm)), sizes[i]];
 }
 
+export function formatGB(bytes: number, decimals = 2) {
+  if (!+bytes) return "0 GB";
+
+  const k = 1024;
+  const dm = decimals < 0 ? 0 : decimals;
+  return `${parseFloat((bytes / Math.pow(k, 3)).toFixed(dm))} GB`;
+}
+
 export const numberWithCommas = (x: number) => {
   if (x !== null) return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 };
